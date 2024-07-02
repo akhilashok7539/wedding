6b7d138cad78665d97546d1b7c8c35a9312d55e1
akhilashok7539 <akhilashok7539@gmail.com>
2024-07-02 12:09:03 +0530

    NEW logic applied on component

--- js/main.js
+++ js/main.js
@@ -42,13 +42,103 @@ $(window).load(function(){
 $(document).ready(function(){
 
 //=====>  Countdown (Edit this with your own date)  <====
-$("#countdown").countdown("2024/08/22 11:30:00", function(event) {
-  var $this = $(this).html(event.strftime(''
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'));
-});
+// $("#countdown").countdown("2024/08/22 11:30:00", function(event) {
+//   var $this = $(this).html(event.strftime(''
+//      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
+//      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
+//      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
+//      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'));
+// });
+// $("#countdown").countdown("2024/03/22 11:30:00", function(event) {
+//   var $this = $(this);
+//   if (event.elapsed) {
+//     // If the countdown has elapsed (reached zero), hide the countdown and show the message
+//     console.log("checks here")
+//     $this.hide();
+//     $("#countdown-message").show(); // Assuming you have a div with id="countdown-message" containing the message
+//     $("#countdown").show();
+//   } else {
+//     console.log("checks here")
+//     // Update the countdown timer
+//     $this.html(event.strftime(''
+//       + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
+//       + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
+//       + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
+//       + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'
+//     ));
+//   }
+// });
+
+
+
+// var targetDate = new Date("2024/03/22 11:30:00");
+
+// // Check if targetDate is in the past
+// if (targetDate < new Date()) {
+//   // If the target date is in the past, show the message immediately
+//   $("#countdown").hide();
+//   $("#countdown-message").show();
+// } else {
+//   // Initialize countdown only if the target date is in the future
+//   $("#countdown").countdown(targetDate, function(event) {
+//     var $this = $(this);
+//     if (event.elapsed) {
+//       // If the countdown has elapsed (reached zero), hide the countdown and show the message
+//       $this.hide();
+//       $("#countdown-message").show();
+//     } else {
+//       // Update the countdown timer
+//       $this.html(event.strftime(''
+//         + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
+//         + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
+//         + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
+//         + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'
+//       ));
+//     }
+//   });
+// }
+
+
+// Function to update UI based on target date
+function updateCountdownUI(targetDate) {
+  var currentDate = new Date();
+  if (targetDate < currentDate) {
+    // If target date is in the past, hide countdown and show message
+    $("#countdown").hide();
+    $("#countdown-message").show();
+  } else {
+    // Initialize or update countdown
+    $("#countdown").countdown(targetDate, function(event) {
+      var $this = $(this);
+      if (event.elapsed) {
+        // If countdown has elapsed, hide countdown and show message
+        $this.hide();
+        $("#countdown-message").show();
+      } else {
+        // Update countdown display
+        $this.html(event.strftime(''
+          + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
+          + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
+          + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
+          + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'
+        ));
+      }
+    });
+  }
+}
+
+// Function to check and update UI every 5 minutes
+function checkAndUpdateCountdown() {
+  var targetDate = new Date("2024/08/22 11:00:00"); // Replace with your target date
+  updateCountdownUI(targetDate);
+}
+
+// Initial call to check and update countdown on page load
+checkAndUpdateCountdown();
+console.log("checks every 5 min span")
+// Schedule periodic checks every 5 minutes
+setInterval(checkAndUpdateCountdown, 5 * 60 * 1000); // 5 minutes in milliseconds
+
 
 
 //Blog item - Hover
